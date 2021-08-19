e7d2598eb0d629d9f4a89f49a50cd02aaef7c10f
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2021-08-19 19:16:57 -0400

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
